0d03cd6e4b23d74cc86388fc7eb396777f0f11a6
soncodeweb <lequangson10@gmail.com>
2023-04-20 21:54:43 +0700

    Exercise deep-equal-compare

---
+++ src/utils/deep-Equal-Compare.ts
@@ -0,0 +1,11 @@
+function deepEqualCompare<T>(
+  a: T extends any[] ? "Dont pass an array here" : T,
+  b: T extends any[] ? "Dont pass an array here" : T
+): boolean {
+  return a === b;
+}
+
+// Dont pass an array here
+deepEqualCompare(true, true);
+deepEqualCompare("quangson", "quangson");
+deepEqualCompare([1, 2], [1, 2]);
